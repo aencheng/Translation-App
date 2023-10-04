d46ebce10bb2af8d5c33b9e7af6f65a82560621b
aencheng <111991942+aencheng@users.noreply.github.com>
2023-10-03 22:47:06 -0400

    Completed. Added Fragment View Binding, Implemented Proper Logic and Code covering Edge Cases. Added Automatic Translation if Source not Selected.

--- app/src/main/java/com/example/project5/MainActivity.kt
+++ app/src/main/java/com/example/project5/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.project5
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.RadioButton
@@ -7,6 +8,8 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.project5.databinding.ActivityMainBinding
 import com.google.mlkit.common.model.DownloadConditions
+import com.google.mlkit.nl.languageid.LanguageIdentification
+import com.google.mlkit.nl.languageid.LanguageIdentificationOptions
 import com.google.mlkit.nl.translate.TranslateLanguage
 import com.google.mlkit.nl.translate.Translation
 import com.google.mlkit.nl.translate.Translator
@@ -15,64 +18,140 @@ import com.google.mlkit.nl.translate.TranslatorOptions
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var sharedViewModel: SharedViewModel
+    // late initialize binding and View Model
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
-
-
         super.onCreate(savedInstanceState)
+
+        // Bind the Activity
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+        // Set view to binding.root
         val view = binding.root
         setContentView(view)
+
+        // Initialize View Model
         sharedViewModel = ViewModelProvider(this)[SharedViewModel::class.java]
+
+        // Replace activity_main fragment with our actual Edit Fragment
         replaceFrag(EditFragment())
-        var source = "English"
-        var translate = "Spanish"
+
+        // Initialize our Language Identifier with a set Confidence Threshold
+        val languageIdentifier = LanguageIdentification.getClient(
+            LanguageIdentificationOptions.Builder()
+                .setConfidenceThreshold(0.34f) // Set confidence threshold for language identification
+                .build()
+        )
+
+        // Initialize our variables we'll be using in translation.
+        var source = ""
+        var translate = ""
         var options: TranslatorOptions?
         var translator: Translator? = null
         var outputString = ""
+
+        // Observes the live data withing the shared View Model
         sharedViewModel.sharedString.observe(this) { newValue ->
-            // Update the TextView with the new value
             outputString = newValue
 
-            if (translator != null) {
+            // Checks that the source is empty and a certain length of characters have been inputted
+            // before running through the auto Translation.
+            if(getTranslateLanguageCode(source) == "NA" && outputString.length >= 3){
+
+                // Begins to Identify our Live Data
+                languageIdentifier.identifyLanguage(outputString)
+                    .addOnSuccessListener { languageCode ->
+                        if (languageCode == "und") {
+                            // Language not identified
+                        } else {// On Success and found a valid Language Code
+                            // pass the value to our source
+                            source = languageCode
+                            // Checks that our source and translate variables are not empty before running this code
+                            // Otherwise, It will crash and throw an error.
+                            if(getTranslateLanguageCode(languageCode) != "NA"
+                                && getTranslateLanguageCode(languageCode) != "NA"){
+                                // Builds our TranslationOptions
+                                options = TranslatorOptions.Builder()
+                                    .setSourceLanguage(getTranslateLanguageCode(source))
+                                    .setTargetLanguage(getTranslateLanguageCode(translate)).build()
+
+                                // Gets the Client of our Options
+                                translator = Translation.getClient(options!!)
+                            }
+                            else{
+                                // If language is not supported, this will be printed.
+                                binding.Output.text = "This language is not supported"
+                            }
+                        }
+                    }
+                    .addOnFailureListener { _ ->
+                        // Handle error
+                    }
+            }
+
+            if(getTranslateLanguageCode(source) != "NA" && translate != ""){
+                // Builds Download Conditions
                 val conditions = DownloadConditions.Builder()
                     .requireWifi()
                     .build()
+                // Checks non-null and the downloads the Model
                 translator!!.downloadModelIfNeeded(conditions)
                     .addOnSuccessListener {
                         // Model downloaded successfully. Okay to start translating.
                         translator!!.translate(newValue)
                             .addOnSuccessListener { translatedText ->
                                 // Translation successful.
+                                // Sets output text to the translation of user input
                                 binding.Output.text = translatedText
                             }
                             .addOnFailureListener { _ ->
                                 // Error.
-                                // ...
                             }
                     }
                     .addOnFailureListener { _ ->
                         // Model couldn’t be downloaded or other internal error.
-                        // ...
                     }
             }
         }
+
+        // Checks for Radio Button Change from Sources
         binding.SourceGroup.setOnCheckedChangeListener{
+            // Sets our radio button based on what has been selected
                 _, checkedId -> val radioButton = view.findViewById<RadioButton>(checkedId)
+            // Sets our source variable to our selected Option
             source = radioButton.text.toString()
-            options = TranslatorOptions.Builder()
-                .setSourceLanguage(getTranslateLanguageCode(source))
-                .setTargetLanguage(getTranslateLanguageCode(translate)).build()
-            translator = Translation.getClient(options!!)
+
+            // Checks for edge case of translate not being selected.
+            // Otherwise would have crashed
+            if(translate != ""){
+                options = TranslatorOptions.Builder()
+                    .setSourceLanguage(getTranslateLanguageCode(source))
+                    .setTargetLanguage(getTranslateLanguageCode(translate)).build()
+                translator = Translation.getClient(options!!)
+            }
+            else{
+                // Asks user to select an option.
+                binding.Output.text = "Please select a Translation Source!"
+            }
         }
 
+        // Checks for Radio Button Change from Translation Sources
         binding.TranslationSource.setOnCheckedChangeListener{
+            // Sets our radio button based on what has been selected
                 _, checkedId -> val radioButton = view.findViewById<RadioButton>(checkedId)
+
+            // Sets our translate variable to our selected Option
             translate = radioButton.text.toString()
-            options = TranslatorOptions.Builder()
-                .setSourceLanguage(getTranslateLanguageCode(source))
-                .setTargetLanguage(getTranslateLanguageCode(translate)).build()
-            translator = Translation.getClient(options!!)
-            if(binding.Output.text.isNotEmpty()){
+
+            // Checks in order to not crash
+            // Builds our TranslationOptions and Translation Client
+            if(source != "") {
+                options = TranslatorOptions.Builder()
+                    .setSourceLanguage(getTranslateLanguageCode(source))
+                    .setTargetLanguage(getTranslateLanguageCode(translate)).build()
+                translator = Translation.getClient(options!!)
+
+                // Begins Translation and Changes text if successful
                 translator!!.translate(outputString)
                     .addOnSuccessListener { translatedText ->
                         // Translation successful.
@@ -86,16 +165,21 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // Function to get languageCode easily
     private fun getTranslateLanguageCode(languageName: String): String {
         return when (languageName) {
             "English" -> TranslateLanguage.ENGLISH
             "Spanish" -> TranslateLanguage.SPANISH
             "German" -> TranslateLanguage.GERMAN
+            "en" -> TranslateLanguage.ENGLISH
+            "es" -> TranslateLanguage.SPANISH
+            "de" -> TranslateLanguage.GERMAN
             // Add cases for other languages as needed
-            else -> throw IllegalArgumentException("Invalid language name: $languageName")
+            else -> "NA"
         }
     }
 
+    // Function to replace our component with the fragment we want
     private fun replaceFrag(fragment: Fragment) {
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
